e939b74a4308a66018bb09bb39ea380c0aed1a0a
David Tulloh <git-david@tulloh.id.au>
2017-08-17 14:52:21 +1000

    Example config.py file

---
+++ config.example.py
@@ -0,0 +1,20 @@
+# This file should be copied to config.py before use
+# The config.py file should not be commited to a public source repository
+# To assist in this config.py has been listed in the .gitignore file
+#
+# This file is exempt from the AGPL requirements so long as it remains
+# a provider of configuration variables and contains no functional code
+#
+# Only accounts whose functionality you rely on are required
+
+twilio_account_sid = ""
+twilio_auth_token = ""
+
+twilio_test_sid = ""
+twilio_test_token = ""
+
+aws_access_key_id = ''
+aws_secret_access_key = ''
+
+didww_account_name = ''
+didww_access_key = ''
